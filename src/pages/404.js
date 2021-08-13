3c667e985dabd0aa01be90a60bcf8277303d69db
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-08-13 13:02:10 +0200

    Remove remaining default styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,24 +2,13 @@ import * as React from 'react'
 
 import GlobalStyle from '../styles/global-style'
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320
-}
-
 const NotFoundPage = () => {
   return (
     <>
       <GlobalStyle />
-      <main style={pageStyles}>
+      <main>
         <title>Not found</title>
-        <h1 style={headingStyles}>Page not found</h1>
+        <h1>Page not found</h1>
       </main>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,13 @@ import * as React from 'react'
 
 import GlobalStyle from '../styles/global-style'
 
-const pageStyles = {
-  color: '#232129',
-  padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif'
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320
-}
-
 const IndexPage = () => {
   return (
     <>
       <GlobalStyle />
-      <main style={pageStyles}>
+      <main>
         <title>Alephium</title>
-        <h1 style={headingStyles}>Alephium</h1>
+        <h1>Alephium</h1>
       </main>
     </>
   )
