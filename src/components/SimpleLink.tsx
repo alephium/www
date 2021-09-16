80247922f87c87302d241f1351f572177604e5f1
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-09-16 16:08:08 +0200

    Darken links on hover

--- src/components/SimpleLink.tsx
+++ src/components/SimpleLink.tsx
@@ -23,6 +23,10 @@ let SimpleLink: FC<SimpleLinkProps> = ({ className, children, to, newTab, text }
 SimpleLink = styled(SimpleLink)`
   text-decoration: none;
   color: ${({ theme, color }) => (color ? color : theme.link)};
+
+  &:hover {
+    filter: brightness(85%);
+  }
 `
 
 export default SimpleLink
