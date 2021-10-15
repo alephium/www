e0856aaf0f91040261f1a9dea2ac3285438aefe4
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-10-15 19:04:24 +0200

    Remove unnecessary inline scripts and styles

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,32 @@
-// const React = require('react')
-
 const PostBodyComponents = [<script async src="/analytics-toggle.js" key="goatcounter-script"></script>]
 
-exports.onRenderBody = ({ setPostBodyComponents }) => {
+export const onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents(PostBodyComponents)
 }
 
 // Remove gatsby-announcer element because it has inline styles (which we don't want due to the CSP)
-exports.wrapRootElement = ({ element }) => {
-  console.log(element.props.children.props.children)
+export const wrapRootElement = ({ element }) => {
   element.props.children = element.props.children.props.children.filter(
     (child) => child.props.id !== 'gatsby-announcer'
   )
   return element
+}
+
+// Remove inline scripts and styles inserted by Gatsby
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+  getPostBodyComponents,
+  replacePostBodyComponents
+}) => {
+  if (process.env.NODE_ENV !== 'production') return
+  replaceHeadComponents(
+    getHeadComponents().filter(
+      (component) =>
+        !['gatsby-image-style-script', 'gatsby-image-style-noscript', 'gatsby-image-style'].includes(component.key)
+    )
+  )
+  replacePostBodyComponents(
+    getPostBodyComponents().filter((component) => !['script-loader', 'chunk-mapping'].includes(component.key))
+  )
 }
