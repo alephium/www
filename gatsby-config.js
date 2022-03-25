3e2c9f8fb0f93fd7935b352f22e419ed0847ff23
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-03-25 12:30:50 +0200

    Change website title tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://alephium.org',
-    title: 'Alephium: Blockchain v3.0',
+    title: 'Alephium: A new paradigm',
     description:
       'A novel sharded blockchain for a secure, scalable and flexible financial ecosystem, empowered by BlockFlow algorithm and extended UTXO model.',
     social: {
