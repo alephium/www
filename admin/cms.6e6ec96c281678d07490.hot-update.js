b3e3393ffb7080138bd31460052b207cafd268fd
mvaivre <m.vaivre@gmail.com>
2022-06-23 15:53:55 +0200

    Updates

---
+++ admin/cms.6e6ec96c281678d07490.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatealephium_website"]("cms",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "cd151e211e9f27a896c2"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.6e6ec96c281678d07490.hot-update.js.map
---
+++ static/879364b4cd6d823f5df0bac37ac93129/Sprout.tsx
@@ -0,0 +1,139 @@
+const Sprout = () => (
+  <svg width={281} height={281} fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#a)">
+      <path
+        d="M81.958 234.167h117.084"
+        stroke="url(#b)"
+        strokeWidth={6}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        shapeRendering="crispEdges"
+      />
+    </g>
+    <g filter="url(#c)">
+      <path
+        d="M117.083 234.167c64.396-29.271 9.367-74.934 35.125-117.084"
+        stroke="url(#d)"
+        strokeWidth={6}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        shapeRendering="crispEdges"
+      />
+    </g>
+    <g filter="url(#e)">
+      <path
+        d="M111.229 110.058c12.879 9.367 21.075 25.759 26.929 43.321-23.416 4.684-40.979 4.684-56.2-3.512-14.05-7.025-26.929-22.246-35.125-49.175 32.784-5.854 51.517 0 64.396 9.366Z"
+        stroke="url(#f)"
+        strokeWidth={6}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        shapeRendering="crispEdges"
+      />
+    </g>
+    <g filter="url(#g)">
+      <path
+        d="M165.087 70.25a81.97 81.97 0 0 0-12.879 46.833c22.246-1.17 38.638-7.025 50.346-16.391 11.708-11.709 18.733-26.93 19.904-53.859-31.612 1.171-46.833 11.709-57.371 23.417Z"
+        stroke="url(#h)"
+        strokeWidth={6}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        shapeRendering="crispEdges"
+      />
+    </g>
+    <defs>
+      <linearGradient id="b" x1={84.5} y1={234} x2={194} y2={234} gradientUnits="userSpaceOnUse">
+        <stop stopColor="#22D877" />
+        <stop offset={1} stopColor="#22A1D8" stopOpacity={0.9} />
+      </linearGradient>
+      <linearGradient id="d" x1={134.646} y1={117.083} x2={134.646} y2={234.167} gradientUnits="userSpaceOnUse">
+        <stop stopColor="#22D877" />
+        <stop offset={0.49} stopColor="#2BC4F4" />
+        <stop offset={1} stopColor="#22D876" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient id="f" x1={92.496} y1={98.348} x2={92.496} y2={156.544} gradientUnits="userSpaceOnUse">
+        <stop stopColor="#22D877" />
+        <stop offset={0.49} stopColor="#2BC4F4" />
+        <stop offset={1} stopColor="#22D876" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient id="h" x1={187.312} y1={46.833} x2={187.312} y2={117.083} gradientUnits="userSpaceOnUse">
+        <stop stopColor="#22D877" />
+        <stop offset={0.49} stopColor="#2BC4F4" />
+        <stop offset={1} stopColor="#22D876" stopOpacity={0} />
+      </linearGradient>
+      <filter
+        id="a"
+        x={74.958}
+        y={231.167}
+        width={131.083}
+        height={14}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_135_246" />
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow_135_246" result="shape" />
+      </filter>
+      <filter
+        id="c"
+        x={110.083}
+        y={114.083}
+        width={49.126}
+        height={131.084}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_135_246" />
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow_135_246" result="shape" />
+      </filter>
+      <filter
+        id="e"
+        x={39.833}
+        y={95.349}
+        width={105.325}
+        height={72.195}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_135_246" />
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow_135_246" result="shape" />
+      </filter>
+      <filter
+        id="g"
+        x={145.165}
+        y={43.833}
+        width={84.293}
+        height={84.25}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+        <feOffset dy={4} />
+        <feGaussianBlur stdDeviation={2} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_135_246" />
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow_135_246" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+)
+
+export default Sprout
