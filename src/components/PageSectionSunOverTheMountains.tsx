9da11797e30fc72ca74f751dd29d7898e2cdeb75
mvaivre <m.vaivre@gmail.com>
2022-06-23 15:27:35 +0200

    Update last illustration, add parallax

--- src/components/PageSectionSunOverTheMountains.tsx
+++ src/components/PageSectionSunOverTheMountains.tsx
@@ -1,26 +1,40 @@
 import styled from 'styled-components'
 
-import SvgSunOverMountains from '../images/sun-over-mountains.svg'
+import SvgSunOverMountainsFront from '../images/sun-over-mountains-front.svg'
+import SvgSunOverMountainsBack from '../images/sun-over-mountains-back.svg'
+import ParallaxWrapper from './ParallaxWrapper'
 
 const PageSectionSunOverTheMountains = () => (
   <Container>
-    <BackgroundImage />
+    <ParallaxImageFront speed={10} />
+    <ParallaxImageBack speed={-2} />
   </Container>
 )
 
 const Container = styled.section`
-  display: flex;
-  justify-content: center;
+  position: relative;
+  height: 600px;
+  width: 100%;
   margin-bottom: var(--spacing-10);
 `
 
-const BackgroundImage = styled.div`
-  width: 70%;
-  height: 400px;
-  background-image: url(${SvgSunOverMountains});
+const ParallaxImage = styled(ParallaxWrapper)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
+`
+
+const ParallaxImageFront = styled(ParallaxImage)`
+  background-image: url(${SvgSunOverMountainsFront});
+`
+
+const ParallaxImageBack = styled(ParallaxImage)`
+  background-image: url(${SvgSunOverMountainsBack});
 `
 
 export default PageSectionSunOverTheMountains
