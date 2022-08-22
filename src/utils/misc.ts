43ce16af35444b6d0737fcc78185605dd7e248fd
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-08-22 10:26:07 +0200

    Add achors to each section title
    Closes #90

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,5 @@
 export const isBrowser = typeof window !== 'undefined'
 
 export const isMobile = isBrowser && /Mobi|Android/i.test(navigator.userAgent)
+
+export const toId = (str: string) => str.replace(/\s/g, '_').toLowerCase()
