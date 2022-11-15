57071c52cf45c3269e03125025cb3385840e7caa
mvaivre <m.vaivre@gmail.com>
2022-11-15 10:34:20 +0100

    Simplify Divider

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components'
 
-interface SectionDividerProps {
-  className?: string
-}
-
-const SectionDivider = ({ className }: SectionDividerProps) => <div className={className} />
-
-export default styled(SectionDivider)`
+export default styled.div`
   height: 1px;
   background-color: ${({ theme }) => theme.separator};
   width: 100%;
