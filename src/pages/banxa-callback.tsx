55050aa0894906de00004b1024de713d6dbff71e
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-01-08 14:57:35 +0800

    Add countdown to redirect to deep link

--- src/pages/banxa-callback.tsx
+++ src/pages/banxa-callback.tsx
@@ -4,20 +4,38 @@ import GlobalStyle from '../styles/global-style'
 import { darkTheme } from '../styles/themes'
 
 import PageSectionContainer from '../components/PageSectionContainer'
+import { useEffect, useState } from 'react'
 
-const BanxaCallbackPage = () => (
-  <ThemeProvider theme={darkTheme}>
-    <GlobalStyle />
-    <main style={{ height: '100vh' }}>
-      <PageSectionContainerStyled>
-        <div className="contents">
-          <h1>Purchace completed! ✅</h1>
-          <h2>The funds will be sent to your wallet shortly.</h2>
-        </div>
-      </PageSectionContainerStyled>
-    </main>
-  </ThemeProvider>
-)
+const CLOSE_BANXA_TAB_DEEP_LINK = 'alephium://close-banxa-tab'
+
+const BanxaCallbackPage = () => {
+  const [countdown, setCountdown] = useState(5)
+
+  useEffect(() => {
+    if (countdown === 0) {
+      window.open(CLOSE_BANXA_TAB_DEEP_LINK, '_self')
+    } else {
+      const interval = setInterval(() => setCountdown(countdown - 1), 1000)
+
+      return () => clearInterval(interval)
+    }
+  }, [countdown])
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <main style={{ height: '100vh' }}>
+        <PageSectionContainerStyled>
+          <div className="contents">
+            <h1>Purchace completed! ✅</h1>
+            <h2>The funds will be sent to your wallet shortly.</h2>
+            {countdown > 0 && <CountdownText>Redirecting to your wallet in {countdown}...</CountdownText>}
+          </div>
+        </PageSectionContainerStyled>
+      </main>
+    </ThemeProvider>
+  )
+}
 
 export default BanxaCallbackPage
 
@@ -25,4 +43,8 @@ const PageSectionContainerStyled = styled(PageSectionContainer)`
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+const CountdownText = styled.h3`
+  color: ${({ theme }) => theme.textSecondary};
 `
