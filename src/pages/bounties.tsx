7d73a4ee68c9c5f7f75fa36f2134325f5a130c1d
mvaivre <m.vaivre@gmail.com>
2025-04-27 14:39:07 +0200

    Greatly simplify bouties page

--- src/pages/bounties.tsx
+++ src/pages/bounties.tsx
@@ -7,6 +7,7 @@ import SubheaderContent from '../components/customPageComponents/SubheaderConten
 import SubpageSection from '../components/customPageComponents/SubpageSection'
 import SubpageVideoHeroSection from '../components/customPageComponents/SubpageVideoHeroSection'
 import TextCard from '../components/customPageComponents/TextCard'
+import TextCardContent from '../components/customPageComponents/TextCardContent'
 import TextElement from '../components/customPageComponents/TextElement'
 import SectionDivider from '../components/SectionDivider'
 import SimpleLink from '../components/SimpleLink'
@@ -39,16 +40,27 @@ const CustomPage = (props: PageProps) => {
             <h1>Alephium Bounty Program</h1>
             <hr />
             <p>
-              As part of our mission to foster innovation and decentralization, Alephium is launching a Bounty Program
-              to reward builders, developers, and researchers who contribute to the growth and security of the Alephium
-              ecosystem.
-            </p>
-            <p>
-              This program is designed to address key development needs, support ecosystem expansion, and encourage
-              community-driven solutions. If you have the skills, knowledge, and vision to enhance Alephium, we invite
-              you to participate, propose new bounties, or take on existing challenges.
+              As part of our mission to foster innovation and decentralization,{' '}
+              <strong>
+                Alephium is launching a Bounty Program to reward builders, developers, and researchers who contribute to
+                the growth and security of the Alephium ecosystem.
+              </strong>
             </p>
           </SubpageVideoHeroSection>
+
+          <SubpageSection>
+            <TextElement>
+              <h2>What is the bounty program?</h2>
+              <p>
+                The bounty program is designed to address key development needs, support ecosystem expansion, and
+                encourage community-driven solutions.{' '}
+                <strong>
+                  If you have the skills, knowledge, and vision to enhance Alephium, we invite you to participate,
+                  propose new bounties, or take on existing challenges.
+                </strong>
+              </p>
+            </TextElement>
+          </SubpageSection>
 
           <SubpageSection>
             <TextElement>
@@ -58,25 +70,31 @@ const CustomPage = (props: PageProps) => {
             <SubheaderContent>
               <Grid columns={3}>
                 <TextCard>
-                  <h3>Suggest a Bounty</h3>
-                  <p>
-                    If you have an idea for a bounty that would help improve the Alephium ecosystem, you can submit your
-                    proposal for review by the Alephium team.
-                  </p>
-                  {/* <Button url="/">Submit a bounty proposal</Button> */}
+                  <TextCardContent>
+                    <h3>Suggest a Bounty</h3>
+                    <p>
+                      If you have an idea for a bounty that would help improve the Alephium ecosystem, you can submit
+                      your proposal for review by the Alephium team.
+                    </p>
+                  </TextCardContent>
                 </TextCard>
                 <TextCard>
-                  <h3>Work on an Existing Bounty</h3>
-                  <p>If you’re ready to take on a bounty, browse the open opportunities and submit your interest.</p>
-                  {/* <Button url="/">View open bounties</Button> */}
+                  <TextCardContent>
+                    <h3>Work on an Existing Bounty</h3>
+                    <p>If you’re ready to take on a bounty, browse the open opportunities and submit your interest.</p>
+                  </TextCardContent>
                 </TextCard>
                 <TextCard>
-                  <h3>Improve Security</h3>
-                  <p>
-                    Security is critical for Alephium. Our Bug Bounty Program rewards contributors who identify and
-                    report vulnerabilities.
-                  </p>
-                  <Button url="https://github.com/alephium/community/blob/master/BugBounty.md">Report a bug</Button>
+                  <TextCardContent>
+                    <h3>Improve Security</h3>
+                    <p>
+                      Security is critical for Alephium. Our Bug Bounty Program rewards contributors who identify and
+                      report vulnerabilities.
+                    </p>
+                    <Button squared url="https://github.com/alephium/community/blob/master/BugBounty.md">
+                      Report a bug
+                    </Button>
+                  </TextCardContent>
                 </TextCard>
               </Grid>
             </SubheaderContent>
@@ -108,7 +126,7 @@ const CustomPage = (props: PageProps) => {
 
           <SectionDivider /> */}
 
-          <SubpageSection>
+          {/* <SubpageSection>
             <TextElement>
               <h2>Bounty Program Process</h2>
             </TextElement>
@@ -121,7 +139,7 @@ const CustomPage = (props: PageProps) => {
                   <ul>
                     <li>Review available bounties and select one that fits your expertise.</li>
                     <li>
-                      Before starting, make sure you meet the program's eligibility requirements and agree to the Terms
+                      Before starting, make sure you meet the program&apos;s eligibility requirements and agree to the Terms
                       & Conditions.
                     </li>
                     <li>
@@ -134,7 +152,7 @@ const CustomPage = (props: PageProps) => {
                 <h3>Step 2</h3>
                 <h4>Submit Your Completed Work</h4>
                 <p>
-                  Once you've completed your bounty task, submit your work through the{' '}
+                  Once you&apos;ve completed your bounty task, submit your work through the{' '}
                   <SimpleLink url="">bounty platform</SimpleLink>.
                 </p>
               </TextElement>
@@ -175,17 +193,19 @@ const CustomPage = (props: PageProps) => {
             </SubheaderContent>
           </SubpageSection>
 
-          <SectionDivider />
+          <SectionDivider /> */}
 
           <SubpageSection>
-            <TextElement>
-              <h2>Need More Info?</h2>
+            <TextElement isCentered>
+              {/* <h2>Ready? Join the Bounty Program today!</h2> */}
+              <h2>The bounty platform is coming soon!</h2>
               <p>
-                For additional details, check out our <SimpleLink url="/faq#bounty-program">FAQ section</SimpleLink> or
-                reach out to our team.
+                We are currently working on building the bounty platform. Please check back soon for updates.{' '}
+                <strong>Until then, you can join the discussion and propose bounties on our channels.</strong>
               </p>
-              <Button url="/contact">Contact us</Button>
-              <Button url="/dicord">Join the discussion</Button>
+              <Button big highlight url="/communities">
+                See our communities
+              </Button>
             </TextElement>
           </SubpageSection>
 
@@ -193,9 +213,12 @@ const CustomPage = (props: PageProps) => {
 
           <SubpageSection>
             <TextElement isCentered>
-              {/* <h2>Ready? Join the Bounty Program today!</h2> */}
-              <h2>The bounty platform is coming soon!</h2>
-              {/* <Button url="/">Join the program</Button> */}
+              <h2>Need More Info?</h2>
+              <p>
+                For additional details, check out our <SimpleLink url="/faq#bounty-program">FAQ section</SimpleLink> or
+                reach out to our team.
+              </p>
+              <Button url="/contact">Contact us</Button>
             </TextElement>
           </SubpageSection>
         </>
