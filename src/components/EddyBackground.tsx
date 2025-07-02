590dcce25d4c9061a43bac5d747b558402df7e14
mvaivre <m.vaivre@gmail.com>
2025-07-02 09:44:03 +0200

    Reduce hero bg width and visibility

--- src/components/EddyBackground.tsx
+++ src/components/EddyBackground.tsx
@@ -19,17 +19,22 @@ export interface MeshGradientEffectProps {
 const MeshGradientEffect: FC<MeshGradientEffectProps> = ({ className }) => {
   const theme = useTheme()
 
-  const leftGradient = `radial-gradient(circle at 0% 50%, ${theme.palette2} 15%, ${theme.palette5} 30%, ${theme.palette1} 50%, ${theme.palette3} 60%, transparent 70%)`
-  const rightGradient = `radial-gradient(circle at 100% 50%, ${theme.palette2} 15%, ${theme.palette5} 30%, ${theme.palette1} 50%, ${theme.palette3} 60%, transparent 70%)`
+  const leftGradient = `radial-gradient(circle at 0% 50%, ${theme.palette2} 15%, ${theme.palette5} 25%, ${theme.palette1} 35%, ${theme.palette3} 40%, transparent 70%)`
+  const rightGradient = `radial-gradient(circle at 100% 50%, ${theme.palette2} 15%, ${theme.palette5} 25%, ${theme.palette1} 35%, ${theme.palette3} 40%, transparent 70%)`
 
   return (
     <EddyBackgroundContainer className={className}>
       <GradientContainer
-        style={{ backgroundImage: leftGradient, left: 0, backgroundPosition: '0% 50%' }}
+        style={{ backgroundImage: leftGradient, left: 0, backgroundPosition: '0% 50%', transformOrigin: '0% 50%' }}
         aria-hidden="true"
       />
       <GradientContainer
-        style={{ backgroundImage: rightGradient, right: 0, backgroundPosition: '100% 50%' }}
+        style={{
+          backgroundImage: rightGradient,
+          right: 0,
+          backgroundPosition: '100% 50%',
+          transformOrigin: '100% 50%'
+        }}
         aria-hidden="true"
       />
     </EddyBackgroundContainer>
@@ -58,9 +63,10 @@ const GradientContainer = styled.div`
   z-index: -1;
   pointer-events: none;
   filter: blur(90px) ${({ theme }) => getColorFilters(theme)};
-  opacity: ${({ theme }) => (theme.name === 'dark' ? 0.4 : 0.8)};
+  opacity: ${({ theme }) => (theme.name === 'dark' ? 0.4 : 0.5)};
   background-size: 65% 75%;
   background-repeat: no-repeat;
+  transform: scaleX(0.7);
 
   @media ${deviceBreakPoints.mobile} {
     filter: blur(70px) ${({ theme }) => getColorFilters(theme)};
