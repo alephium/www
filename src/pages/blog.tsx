d7402f7c8d636b3af695795d4db2961aeb447617
mvaivre <m.vaivre@gmail.com>
2025-09-05 09:42:15 +0200

    Improve blog page layout

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,7 +5,6 @@ import styled from 'styled-components'
 import Grid from '../components/customPageComponents/Grid'
 import Page from '../components/customPageComponents/Page'
 import SubpageSection from '../components/customPageComponents/SubpageSection'
-import TextCard from '../components/customPageComponents/TextCard'
 import TextElement from '../components/customPageComponents/TextElement'
 import GatsbyImageWrapper from '../components/GatsbyImageWrapper'
 import Search from '../components/Search'
@@ -67,14 +66,12 @@ const CustomPage = (props: PageProps) => {
       }}
       content={
         <>
-          <SubpageSection fullWidth>
+          <SubpageSection>
             <TextElement isCentered>
               <h1>Alephium blog</h1>
               <p>News, updates, and insights from the Alephium ecosystem.</p>
             </TextElement>
-          </SubpageSection>
 
-          <SubpageSection wide>
             <SearchContainer>
               <Search value={searchQuery} onChange={setSearchQuery} placeholder="Search articles..." />
             </SearchContainer>
@@ -87,21 +84,7 @@ const CustomPage = (props: PageProps) => {
             ) : (
               <Grid columns={3} gap="large">
                 {filteredPosts.map((post) => (
-                  <TextCardStyled key={post.fields?.slug} url={post.fields?.slug ?? ''} border>
-                    <TextElement style={{ padding: '20px' }}>
-                      <h3>{post.frontmatter?.title}</h3>
-                      <p>{post.frontmatter?.description}</p>
-                    </TextElement>
-                    {post.frontmatter?.featuredImage && (
-                      <ImageContainer>
-                        <GatsbyImageWrapper
-                          image={post.frontmatter.featuredImage.childImageSharp?.gatsbyImageData}
-                          alt={post.frontmatter?.title ?? ''}
-                          style={{ width: '100%', height: '200px' }}
-                        />
-                      </ImageContainer>
-                    )}
-                  </TextCardStyled>
+                  <BlogCard key={post.fields?.slug} post={post} />
                 ))}
               </Grid>
             )}
@@ -112,24 +95,46 @@ const CustomPage = (props: PageProps) => {
   )
 }
 
-const BlogCard = styled.div``
+// TODO: Extract to separate file if used in other places
+interface BlogCardProps {
+  post: Queries.BlogPostsQuery['allMarkdownRemark']['nodes'][number]
+}
+
+const BlogCard = ({ post }: BlogCardProps) => {
+  if (!post.frontmatter?.featuredImage) {
+    return null
+  }
+
+  return (
+    <BlogCardContainer>
+      <ImageContainer>
+        <GatsbyImageWrapper
+          image={post.frontmatter.featuredImage.childImageSharp?.gatsbyImageData}
+          alt={post.frontmatter.title ?? ''}
+          style={{ width: '100%', height: '200px' }}
+        />
+      </ImageContainer>
+      <TextElement isBodySmall>
+        <h4 style={{ marginBottom: '10px' }}>{post.frontmatter.title}</h4>
+        <label>{post.frontmatter.date}</label>
+        <p>{post.frontmatter.description}</p>
+      </TextElement>
+    </BlogCardContainer>
+  )
+}
 
 export default CustomPage
-
-const TextCardStyled = styled(TextCard)`
-  display: flex;
-  flex-direction: column-reverse;
-  gap: var(--spacing-4);
-  justify-content: space-between;
-`
 
 const ImageContainer = styled.div`
   width: 100%;
   position: relative;
+  border-radius: var(--radius);
+  overflow: hidden;
 `
 
 const SearchContainer = styled.div`
   margin-bottom: var(--spacing-8);
+  margin-top: var(--spacing-8);
 `
 
 const NoResults = styled.div`
@@ -140,4 +145,10 @@ const NoResults = styled.div`
   h3 {
     margin-bottom: var(--spacing-4);
   }
+`
+
+const BlogCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing-4);
 `
