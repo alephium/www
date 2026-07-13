862ec5b2da875cabe4224dffeb6326b5497394cd
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-07-13 10:00:07 +0200

    Hide Reddit button
    Remove the Reddit entry from the socials list, which drives the social
    buttons on the communities page. Also drop the now-unused Reddit icon
    mapping and SVG asset.

--- src/images/utils.ts
+++ src/images/utils.ts
@@ -2,7 +2,6 @@ import DiscordIcon from './svgs/brand-icon-discord.svg'
 import GitHubIcon from './svgs/brand-icon-github.svg'
 import LinkedInIcon from './svgs/brand-icon-linkedin.svg'
 import MediumIcon from './svgs/brand-icon-medium.svg'
-import RedditIcon from './svgs/brand-icon-reddit.svg'
 import TelegramIcon from './svgs/brand-icon-telegram.svg'
 import TwitterIcon from './svgs/brand-icon-X.svg'
 import YouTubeIcon from './svgs/brand-icon-youtube.svg'
@@ -15,8 +14,6 @@ export const getIconByName = (name: string) => {
       return TelegramIcon
     case 'Twitter/X':
       return TwitterIcon
-    case 'Reddit':
-      return RedditIcon
     case 'Medium':
       return MediumIcon
     case 'LinkedIn':
